f258bc43b15571245256c17b4afcce2ce1a59be3
codesudani <37102761+codesudani@users.noreply.github.com>
2021-09-02 00:28:49 +0300

    Add files via upload
    Arithmetic operators are used to perform basic mathematical operations such as addition (+), subtraction (-), multiplication (*), division (/) etc.

---
+++ ArithmeticOperations.kt
@@ -0,0 +1,24 @@
+
+fun main(args : Array<String>) {  
+
+// Arithmetic operations in kotlin with examples
+
+var a=10;  
+var b=5;  
+
+//a.plus(b)
+println(a+b);  
+
+//a.minus(b)
+println(a-b);  
+
+//a.times(b)
+println(a*b);  
+
+//a.div(b)
+println(a/b);  
+
+//a.rem(b)
+println(a%b);  
+
+}  
