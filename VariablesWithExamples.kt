fee64126343a1d31f43528bdbafd2595e1244964
codesudani <37102761+codesudani@users.noreply.github.com>
2021-09-02 00:13:46 +0300

    Add files via upload
    امثلة لأنواع المتغيرات في الكوتلن مع تعليقات توضيحية

---
+++ VariablesWithExamples.kt
@@ -0,0 +1,34 @@
+// Syntax :
+// var variableName = value 
+
+var name = "John"      // String (text)
+val birthyear = 1975   // Int (number)
+
+println(name)          // Print the value of name
+println(birthyear)     // Print the value of birthyear
+
+//Or 
+
+var name: String = "John" // String
+val birthyear: Int = 1975 // Int
+
+println(name)
+println(birthyear)
+
+//Other example
+
+val pi = 3.14159265359
+println(pi)
+
+//Display Variables
+
+val name = "John"
+println("Hello " + name)
+
+//Or
+
+val firstName = "John "
+val lastName = "Doe"
+val fullName = firstName + lastName
+println(fullName)
+
